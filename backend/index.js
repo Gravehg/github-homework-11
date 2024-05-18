b1c58cc01ab02be9aee21ca187e906cf603bd90b
Gravehg <80481681+Gravehg@users.noreply.github.com>
2024-05-17 22:30:20 -0600

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,9 @@
-console.log("Hello world");
+const express = require("express");
+
+const app = express();
+
+app.listen(3000, () => {});
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
